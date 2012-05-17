1793b7c15e36e80c2e21902f7510918113fa019f
Hash <takuya21hashimoto@gmail.com>
2012-05-17 14:39:48 +0900

    prepare spec to remove @ mark from tags to avoid seinding mentions on TimeLine

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -5,19 +5,25 @@ describe 'Bookmark' do
   it { Bookmark.class.should eq Class }
 
   describe '#tagmapper' do
-    before(:all) do
-      @b = Bookmark.create({
-        title: "twitter bootstrap railsを使ったら職が見つかり彼女も出来て - ppworks blog",
-        link:  "http://ppworks.hatenablog.jp/entry/2012/02/19/033644",
-        blink: "http://b.hatena.ne.jp/Hash/20120307#bookmark-81508937",
-        time: "2012-03-07 03:11:35",
-        bcnt: 49,
-        tags: ["hoge".as_a_tag, "@sotarok".as_a_tag]
-      })
-    end
+    let(:base_params) {{
+      title: "twitter bootstrap railsを使ったら職が見つかり彼女も出来て - ppworks blog",
+      link:  "http://ppworks.hatenablog.jp/entry/2012/02/19/033644",
+      blink: "http://b.hatena.ne.jp/Hash/20120307#bookmark-81508937",
+      time: "2012-03-07 03:11:35",
+      bcnt: 49
+    }}
 
-    it { Bookmark.count.should eq 1 }
-    #it 'should remove @ when tag is @someone' do
-    #end
+    it 'should remove @ when tag is @someone' do     
+      params = base_params.merge( tags: ["hoge".as_a_tag, "fuga".as_a_tag] )
+      Bookmark.create(params)
+      b = Bookmark.first
+      b.tagmapper.should eq "hoge,fuga"
+    end
+    it 'should remove @ when tag is @someone' do     
+      params = base_params.merge( tags: ["hoge".as_a_tag, "@ppworks".as_a_tag] )
+      Bookmark.create(params)
+      b = Bookmark.first
+      b.tagmapper.should eq "hoge,ppworks"
+    end
   end
 end
